77dda18878d27d346384e3d06a9ef8cb2075b66f
Jiajie Yao <yaojiajie@hotmail.com>
2020-06-04 12:32:06 +0800

    use finetuning instead of training from scratch.

--- official/resnet/train.sh
+++ official/resnet/train.sh
@@ -1,2 +1,5 @@
-python imagenet_main.py --data_dir=/test/data/datasets/imagenet_tfrecord/  --model_dir=/test/data/jiajie/resnet_checkpoint/ --batch_size=64 export_dir=/test/data/jiajie/resnet_model/ --data_format channels_last  --clean
+python imagenet_main.py --data_dir=/test/data/datasets/imagenet_tfrecord/  --model_dir=/test/data/jiajie/resnet_checkpoint/ --batch_size=64 export_dir=/test/data/jiajie/resnet_model/ --data_format channels_last  --fine_tune --pretrained_model_checkpoint_path=/test/data/jiajie/resnet_imagenet_v1_fp32_20181001/model.ckpt-222860 --clean
+
+
+
 
